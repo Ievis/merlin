b67aac1ee4be7a47e759496eec5815cda534e5c6
Ievis <levchaba@gmail.com>
2023-05-12 22:23:42 +0600

    .

--- src/Controller/FooController.php
+++ src/Controller/FooController.php
@@ -1,6 +1,18 @@
 <?php
 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
 class FooController
 {
+    public function index(Request $request)
+    {
+        dump('index');
+    }
 
+    public function load(Request $request, $id)
+    {
+        dump('load');
+    }
 }
--- web/app.php
+++ web/app.php
@@ -1,3 +1,69 @@
 <?php
 
-echo 123;
+require '../vendor/autoload.php';
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use App\Controller\FooController;
+
+
+try {
+    $request = Request::createFromGlobals();
+
+    $containerBuilder = new ContainerBuilder();
+    $containerBuilder->register(FooController::class, FooController::class);
+
+    $definition = new Definition(FooController::class);
+
+    $fileLocator = new FileLocator(__DIR__ . '/../config');
+    $loader = new YamlFileLoader($fileLocator);
+    $routes = $loader->load('routes.yaml');
+
+    $context = new RequestContext();
+    $context->fromRequest($request);
+
+    $matcher = new UrlMatcher($routes, $context);
+    $parameters = $matcher->match($context->getPathInfo());
+
+    $controller_info = explode('::', $parameters['_controller']);
+
+    $controller = $controller_info[0];
+    $action = $controller_info[1];
+
+    $vars = array_diff($parameters, [
+        '_route' => $parameters['_route'],
+        '_controller' => $parameters['_controller']
+    ]);
+
+    $method_call_definitions = [
+        FooController::class => [
+            'load' => [
+                'request' => $request,
+                'id' => $vars['id'] ?? null
+            ],
+            'index' => [
+                'request' => $request
+            ]
+        ]
+    ];
+
+    $definition->addMethodCall($action, $method_call_definitions[$controller][$action]);
+
+
+    $containerBuilder->addDefinitions([
+        FooController::class => $definition
+    ]);
+
+    $containerBuilder->get((new $controller)::class);
+
+} catch (ResourceNotFoundException $e) {
+    return json_encode([
+        'success' => false
+    ]);
+}
